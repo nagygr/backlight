67c7df192d9bb8fd61e65a4a81c5b2d74a978742
Gergely Nagy <nagygr@gmail.com>
2023-01-12 15:34:24 +0100

    [ADD] Getting max brightness and setting brightness to a value.

--- cmd/backlight/main.go
+++ cmd/backlight/main.go
@@ -8,14 +8,20 @@ import (
 )
 
 const (
+	brightnessCmd       = "/sys/class/backlight/intel_backlight/brightness"
 	actualBrightnessCmd = "/sys/class/backlight/intel_backlight/actual_brightness"
+	maxBrightnessCmd    = "/sys/class/backlight/intel_backlight/max_brightness"
 )
 
 func main() {
 	var (
-		brightnessCtrl = hw.NewBrightnessController(actualBrightnessCmd)
-		brightness     int
-		err            error
+		brightness    int
+		maxBrightness int
+		err           error
+
+		brightnessCtrl = hw.NewBrightnessController(
+			brightnessCmd, actualBrightnessCmd, maxBrightnessCmd, 0,
+		)
 	)
 
 	brightness, err = brightnessCtrl.CurrentBrightness()
@@ -25,4 +31,15 @@ func main() {
 
 	fmt.Printf("Brightness: %d\n", brightness)
 
+	maxBrightness, err = brightnessCtrl.MaxBrightness()
+	if err != nil {
+		log.Fatalf("Error acquiring max brightness: %s", err)
+	}
+
+	fmt.Printf("Max brightness: %d\n", maxBrightness)
+
+	err = brightnessCtrl.SetBrightness(1400)
+	if err != nil {
+		log.Fatalf("Error setting brightness: %s", err)
+	}
 }
--- pkg/hw/brightness.go
+++ pkg/hw/brightness.go
@@ -2,25 +2,36 @@ package hw
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strconv"
 	"strings"
 )
 
 type BrightnessController struct {
-	ActualBrightnessCmd string
+	brightnessCmd       string
+	actualBrightnessCmd string
+	maxBrightnessCmd    string
+	maxBrightness       int
 }
 
-func NewBrightnessController(actualBrightnessCmd string) *BrightnessController {
+func NewBrightnessController(
+	brightnessCmd string,
+	actualBrightnessCmd string,
+	maxBrightnessCmd string,
+	maxBrightness int,
+) *BrightnessController {
 	return &BrightnessController{
-		ActualBrightnessCmd: actualBrightnessCmd,
+		brightnessCmd:       brightnessCmd,
+		actualBrightnessCmd: actualBrightnessCmd,
+		maxBrightnessCmd:    maxBrightnessCmd,
 	}
 }
 
 func (b *BrightnessController) CurrentBrightness() (int, error) {
 	var (
 		actualBrightnessCommand = exec.Command(
-			"cat", b.ActualBrightnessCmd,
+			"cat", b.actualBrightnessCmd,
 		)
 		output []byte
 		err    error
@@ -44,4 +55,60 @@ func (b *BrightnessController) CurrentBrightness() (int, error) {
 	}
 
 	return brightness, nil
+}
+
+func (b *BrightnessController) MaxBrightness() (int, error) {
+	if b.maxBrightness == 0 {
+		var (
+			maxBrightnessCommand = exec.Command(
+				"cat", b.maxBrightnessCmd,
+			)
+			output []byte
+			err    error
+		)
+
+		output, err = maxBrightnessCommand.CombinedOutput()
+		if err != nil {
+			return 0, fmt.Errorf("Couldn't run max brightness command: %s", err)
+		}
+
+		var (
+			maxBrightnessStr = strings.TrimSpace(string(output))
+			maxBrightness    int
+		)
+		maxBrightness, err = strconv.Atoi(maxBrightnessStr)
+		if err != nil {
+			return 0, fmt.Errorf(
+				"Couldn't convert the max brightness output (%s) to a number: %s",
+				maxBrightnessStr, err,
+			)
+		}
+
+		b.maxBrightness = maxBrightness
+	}
+
+	return b.maxBrightness, nil
+}
+
+func (b *BrightnessController) SetBrightness(value int) error {
+	var (
+		maxBrightness int
+		err           error
+	)
+
+	maxBrightness, err = b.MaxBrightness()
+	if err != nil {
+		return fmt.Errorf("Error acquiring max brightness: %w", err)
+	}
+
+	if value < 0 || value > maxBrightness {
+		return fmt.Errorf("Value (%d) out of range [0, %d]", value, maxBrightness)
+	}
+
+	err = os.WriteFile(b.brightnessCmd, []byte(fmt.Sprintf("%d", value)), 0777)
+	if err != nil {
+		return fmt.Errorf("Couldn't run brightness command: %s", err)
+	}
+
+	return nil
 }
